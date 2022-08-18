7e695902b1018e71f7ae2cc8ce473e331972e34e
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-08-19 01:48:16 +0530

    Add files via upload

---
+++ volume of cuboid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+float  a,b,c,d;
+printf("Enter the length,width and height of the cuboid");
+scanf("%f %f %f",&a,&b,&c);
+d=a*b*c;
+printf("The volume of the cuboid is %f",d);
+getch();
+}
+
+
