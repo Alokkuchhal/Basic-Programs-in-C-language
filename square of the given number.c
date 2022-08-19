8f1c9799ac5cc753e514d170a656548af29e1650
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-08-19 12:36:02 +0530

    Add files via upload

---
+++ square of the given number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float a;
+    printf("Enter a number");
+    scanf("%f",&a);
+    printf("The square of the given number is %f",a*a);
+    getch();
+}
