48370799932820a3eae397b21e279af6a6ba47b5
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-08-19 13:12:08 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter the legth & width of the rectangle");
+    scanf("%f %f",&a,&b);
+    c=a*b;
+    printf("The area of the rectangle is %f",c);
+    getch();
+}
