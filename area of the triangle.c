8ce1ae1e7a5952453fda29c33decc5015a7457f5
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-08-20 12:16:16 +0530

    Add files via upload

---
+++ area of the triangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter the base and height of the triangle .");
+    scanf("%f %f",&a,&b);
+    c=(a*b)/2;
+    printf("The area of the triangle is %f.",c);
+    getch();
+
+}
