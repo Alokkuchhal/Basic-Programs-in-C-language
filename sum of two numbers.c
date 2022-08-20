8ee24761a4bc593e0bc27465b2cde9a5d2643da3
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-08-20 12:04:39 +0530

    Add files via upload

---
+++ sum of two numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float a,b,c;
+    printf("Enter two numbers");
+    scanf("%f %f",&a,&b);
+    c=a+b;
+    printf("The sum of the two enterd numbers is %f",c);
+    getch();
+    return 0;
+}
