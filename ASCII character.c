1ca9f1a312ed936e120ece78de0e0a83ccfa23d7
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-08-20 14:05:24 +0530

    Add files via upload

---
+++ ASCII character.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+# include<conio.h>
+int main()
+{
+   char c;
+   printf("Enter any character what you want to know the ASCII number :");
+   scanf("%c",&c);
+   printf("THE ASCII character of %c is %d",c,c);
+   getch();
+   return 0;
+}
