b4b48bdceaa6d3eb496fd4e296a44536393dcf37
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-09-04 00:12:44 +0530

    Add files via upload

---
+++ mathod 02 of swap values of two int varaibles.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter any two numbers which you want to swap:");
+    scanf("%d %d",&a,&b);
+      printf("The swaped values are %d and %d",b,a);
+    return 0;
+    getch();
+    }
