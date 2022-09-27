89328fd0af2e04d222eb66c4f7667fc548b7205b
Alokkuchhal2004 <110560883+Alokkuchhal2004@users.noreply.github.com>
2022-09-27 22:10:42 +0530

    Add files via upload

---
+++ firrst 10 natural numbers using loop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{   int N,i=1;
+    printf("Enter a number:");
+    scanf("%d",&N);
+    while(i<=N)
+    {
+        printf("%d\n",i);
+        i=i+1;
+    }
+    return 0;
+    getch();
+}
